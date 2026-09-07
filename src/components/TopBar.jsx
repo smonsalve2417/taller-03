e84ed15864b86fa1f125af423b8b8918710b0606
Sebastian Monsalve Otero <87137700+smonsalve2417@users.noreply.github.com>
2026-09-07 15:19:57 -0500

    Barra Superior

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,14 @@
+function TopBar() {
+  return (
+    <nav className="topBar">
+          <p className="logo">ReactAcademy</p>
+          <div className="menu">
+            <a >Inicio</a>
+            <a >Cursos</a>
+            <a >Nosotros</a>
+          </div>
+    </nav>
+  )
+}
+
+export default TopBar
