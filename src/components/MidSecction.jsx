b993e3363df70d55f99ebf633e1c184cba03801e
smonsalve2417 <xxsebasxx24@gmail.com>
2026-09-09 20:31:18 -0500

    Create MidSecction.jsx

---
+++ src/components/MidSecction.jsx
@@ -0,0 +1,13 @@
+function MidSecction() {
+  return (
+    <div className="midSection">
+      <p className="heading">Aprende React desde cero</p>
+      <p className="subheading">Domina la libreria mas popular del frontend con proyectos<br></br> practicos y reales</p> 
+      <button  className="button">Ver Cursos</button>
+    </div>
+  )
+}
+
+export default MidSecction
+
+//hero
